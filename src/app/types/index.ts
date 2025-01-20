d941f1deedfd24b4ad80dbce7a17f96074503f81
Artyom Ulanchik <artyom.u@outlook.com>
2025-01-20 17:41:01 +0000

    creating servercard component

---
+++ src/app/types/index.ts
@@ -0,0 +1,13 @@
+interface ServerData {
+    id: number;
+    name: string;
+    game: string;
+    players: string;
+    status: string;
+    version: string;
+    type: string;
+    region: string;
+    mods: string[];
+}
+
+export type { ServerData }
